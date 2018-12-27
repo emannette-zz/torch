a9c576fe311cef7ace02daa82f480cf644b79a40
Ethan Mannette <ethanmannette@gmail.com>
2018-12-26 19:46:27 -0700

    Finished the data loading/prep tutorial

--- dataLoading/dataPrep.py
+++ dataLoading/dataPrep.py
@@ -257,3 +257,17 @@ for i_batch, sample_batched in enumerate(dataloader):
         plt.ioff()
         plt.show()
         break
+
+
+# data_transform = transforms.Compose([
+#         transforms.RandomSizedCrop(224),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+#                                 std=[0.229, 0.224, 0.225])
+#     ])
+# hymenoptera_dataset = datasets.ImageFolder(root='hymenoptera_data/train',
+#                                             transform=data_transform)
+# dataset_loader = torch.utils.data.DataLoader(hymenoptera_dataset,
+#                                             batch_size=4, shuffle=True,
+#                                             num_workers=4)
